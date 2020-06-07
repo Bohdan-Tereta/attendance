f309516aadb2c10a7401c9d6f657138d6712ab3b
Bohdan Tereta <bohdan.tereta@gmail.com>
2020-06-07 18:44:14 +0300

    made admin more user-friendly

--- attendance/mainApp/admin.py
+++ attendance/mainApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from.models import Device, Waypoint
+from.models import Device, Minor, MinorWaypointHistory, Waypoint
 
 # Register your models here.
 
@@ -12,5 +12,23 @@ class DeviceAdmin(admin.ModelAdmin):
             obj.creator = request.user
         obj.updater = request.user
         super().save_model(request, obj, form, change)
+    list_display = ('id', 'code', 'device_type', 'description')
 
-@admin.site.register(Waypoint)
+@admin.register(Minor)
+class MinorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_id', 'current_grade')
+
+@admin.register(MinorWaypointHistory)
+class MinorWaypointHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'minor_id', 'waypoint_id', 'created_at')
+
+@admin.register(Waypoint)
+class WaypointAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', 'created_at', 'updated_at','creator', 'updater')
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            # Only set added_by during the first save.
+            obj.creator = request.user
+        obj.updater = request.user
+        super().save_model(request, obj, form, change)
+    list_display = ('id', 'code', 'address', 'device_id', 'description')
--- attendance/mainApp/models.py
+++ attendance/mainApp/models.py
@@ -4,7 +4,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Device(models.Model):
-    code = models.CharField(max_length=50)
+    code = models.CharField(
+        max_length=50, 
+        unique = True,
+        )
     description  = models.CharField(max_length=50)
     device_type  = models.CharField(max_length=50)  
     disabled = models.BooleanField()
@@ -20,13 +23,18 @@ class Device(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.code
 
 class Waypoint(models.Model):
     device_id = models.OneToOneField(
         Device,
         models.CASCADE
     )
-    code = models.CharField(max_length=50)
+    code = models.CharField(
+        max_length=50,
+        unique=True
+    )
     description  = models.CharField(max_length=50)
     address  = models.CharField(max_length=50)
     disabled = models.BooleanField()
@@ -42,6 +50,8 @@ class Waypoint(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.address
 
 class Minor(models.Model):
     user_id = models.ForeignKey(
@@ -49,6 +59,8 @@ class Minor(models.Model):
         models.CASCADE
     )
     current_grade = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return User.objects.filter(id=1).first().username
 
 class MinorWaypointHistory(models.Model):
     minor_id = models.ForeignKey(
