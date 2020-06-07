0ecbf4f5f770ef767c2731750686bf994f940c0c
Bohdan Tereta <bohdan.tereta@gmail.com>
2020-06-07 16:23:16 +0300

    added Waypoint model

--- attendance/mainApp/models.py
+++ attendance/mainApp/models.py
@@ -20,3 +20,25 @@ class Device(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Waypoint(models.Model):
+    device_id = models.ForeignKey(
+        Device,
+        models.CASCADE
+    )
+    code = models.CharField(max_length=50)
+    description  = models.CharField(max_length=50)
+    address  = models.CharField(max_length=50)
+    disabled = models.BooleanField()
+    creator = models.ForeignKey(
+        User,
+        models.CASCADE,
+        'waypoint_to_user_created'
+    )
+    updater = models.ForeignKey(
+        User,
+        models.CASCADE,
+        'waypoint_to_user_added'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
