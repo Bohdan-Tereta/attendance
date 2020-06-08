58c0e0ace8bc33d5e3b3cdda53b08d24195e0c83
Bohdan Tereta <bohdan.tereta@gmail.com>
2020-06-08 15:12:59 +0300

    simple app to read waypoints

---
+++ frontend/attendance/src/components/dashboard.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const apiEndpoint = "http://localhost:8000/app/api/minor-waypoint-history";
+const pollingInterval = 5000;
+
+export function Dashboard() {
+  const [minorWaypointHistory, setMinorWaypointHistory] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      await getMinorWaypointHistory();
+    }
+    fetchData();
+  }, []);
+
+  async function getMinorWaypointHistory() {
+    try {
+      const response = await axios.get(apiEndpoint);
+      setMinorWaypointHistory(response.data);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+    setTimeout(getMinorWaypointHistory, pollingInterval)
+  }
+
+  return (
+    <div>
+      <ul>
+        {minorWaypointHistory.map(entry =>
+          <li key={entry.id}>
+            {JSON.stringify(entry)}
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+}
